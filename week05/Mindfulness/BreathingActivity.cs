5a12f45cc025861c4fb4c553341a898180209435
qwes3d <uratation@gmail.com>
2025-04-01 01:31:23 +0000

    Validate duration for Breathing Activity and enhance user feedback

--- week05/Mindfulness/BreathingActivity.cs
+++ week05/Mindfulness/BreathingActivity.cs
@@ -1,4 +1,5 @@
-
+using System;
+using System.Threading;
 
 // Breathing Activity
 class BreathingActivity : Activity
@@ -8,13 +9,30 @@ class BreathingActivity : Activity
     public override void PerformActivity()
     {
         StartActivity();
-        for (int i = 0; i < Duration; i += 6)
+
+        if (Duration < 3)
+        {
+            Console.WriteLine("Duration is too short for this activity. Please choose at least 3 seconds.");
+            return;
+        }
+
+        Console.WriteLine("\nGet ready to begin...\n");
+        Thread.Sleep(1000);
+
+        int remainingTime = Duration;
+        while (remainingTime > 0)
         {
             Console.WriteLine("Breathe in...");
-            ShowAnimation(3);
+            ShowAnimation(Math.Min(3, remainingTime));
+            remainingTime -= 3;
+
+            if (remainingTime <= 0) break;
+
             Console.WriteLine("Breathe out...");
-            ShowAnimation(3);
+            ShowAnimation(Math.Min(3, remainingTime));
+            remainingTime -= 3;
         }
+
         EndActivity();
     }
 }
